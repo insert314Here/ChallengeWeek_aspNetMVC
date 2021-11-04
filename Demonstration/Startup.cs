99a1905d27e8d2d62350a76274eba6161c6ac8cc
insert314Here <89361627+insert314Here@users.noreply.github.com>
2021-11-04 10:25:15 -0400

    added a simple view as a demonstartion, no real changes to webapp functions

--- Demonstration/Startup.cs
+++ Demonstration/Startup.cs
@@ -119,4 +119,4 @@ namespace Demonstration
 
     }
 }
-}
+
---
+++ TemplateInheritance/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TemplateInheritance.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
